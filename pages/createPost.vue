c9aa5016a3e5d2abaf32c36c26e0ae4c3dbbf030
jacksonPrimo <jackson.primo12@gmail.com>
2023-11-25 08:50:42 -0300

    adjust image name

--- pages/createPost.vue
+++ pages/createPost.vue
@@ -59,7 +59,7 @@ export default {
     },
     uploadFile(image){
       return new Promise((resolve, reject)=>{
-        const path = `${this.$store.state.auth.uid}/${this.image.name}`
+        const path = `${this.$store.state.auth.uid}/${this.randomId()}`
         const storageRef = this.$fire.storage.ref(path).put(image);
         storageRef.on(`state_changed`, snapshot=>{
           this.uploadValue = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
@@ -72,6 +72,18 @@ export default {
           });
         });
       })
+    },
+    randomId() {
+      let result = '';
+      const characters = '0123456789';
+      const charactersLength = characters.length;
+
+      for (let i = 0; i < 14; i++) {
+        const randomIndex = Math.floor(Math.random() * charactersLength);
+        result += characters.charAt(randomIndex);
+      }
+
+      return result;
     }
   }
 }
