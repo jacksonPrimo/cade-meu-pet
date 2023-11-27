f001ee560afb6bca41140b9fec0ba0a63b67bc74
jacksonPrimo <jackson.primo12@gmail.com>
2023-11-26 21:24:44 -0300

    adjust address select in location step

--- components/Steps/Location.vue
+++ components/Steps/Location.vue
@@ -9,6 +9,39 @@
   
     <v-stepper-content :step="step">
       <div ref="map" class="map-container"></div>
+      <v-form ref="addressForm" v-model="formValid" class="mt-4">
+        <v-text-field
+          label="Cidade"
+          v-model="address.city"
+          :rules="[ value => value ? true : 'Cidade é obrigatório' ]"
+          outlined
+          dense
+        ></v-text-field>
+
+        <v-text-field
+          label="Bairro"
+          v-model="address.district"
+          :rules="[ value => value ? true : 'Bairro é obrigatório' ]"
+          outlined
+          dense
+        ></v-text-field>
+        
+        <v-text-field
+        label="Rua"
+        v-model="address.street"
+        :rules="[ value => value ? true : 'Rua é obrigatório' ]"
+        outlined
+        dense
+        ></v-text-field>
+
+        <v-text-field
+          label="Cep"
+          v-model="address.zipcode"
+          :rules="[ value => value ? true : 'Cep é obrigatório' ]"
+          outlined
+          dense
+        ></v-text-field>
+      </v-form>
       <v-btn color="primary" @click="next">
         Continue
       </v-btn>
@@ -31,7 +64,18 @@ export default {
   data: () => ({
     step: 4,
     map: null,
-    location: null
+    marker: null,
+    location: null,
+    formValid: false,
+    address: {
+      state: '',
+      district: '',
+      city: '',
+      street: '',
+      zipcode: '',
+      lat: '',
+      lng: ''
+    }
   }),
   props: {
     currentStep: {
@@ -44,14 +88,13 @@ export default {
   },
   methods: {
     next(){
-      if(this.location){
+      this.$refs.addressForm.validate()
+      if(this.valid) {
         this.$store.dispatch('post/setPostToCreate', {
-          addressLabel: this.location.label,
-          addressLong: this.location.x, 
-          addressLat: this.location.y
+          address: this.address
         })
         this.$emit("next")
-      } else {
+      } else if(this.address.lat || this.address.lng){
         alert('selecione uma localização')
       }
     },
@@ -69,13 +112,58 @@ export default {
         style: 'bar',
         autoComplete: true,
         autoClose: true,
-        keepResult: true
+        keepResult: true,
       })
       this.map.on('geosearch/showlocation', this.handleSelectedLocation)
       this.map.addControl(searchControl)
+      this.map.on('click', (e) => this.selectPosition(e.latlng.lat, e.latlng.lng));
     },
     handleSelectedLocation(e){
-      this.location = e.location
+      this.map.removeLayer(e.marker);
+      this.selectPosition(e.location.y, e.location.x)
+      const splittedInfo = e.location.label.split(',')
+      if(splittedInfo.length == 8) {
+        this.address = {
+          street: "",
+          state: splittedInfo[4],
+          district: splittedInfo[0],
+          city: splittedInfo[1],
+          zipcode: splittedInfo[6],
+          lat: e.y,
+          lng: e.x,
+        }
+      } else if(splittedInfo.length == 6) {
+        this.address = {
+          street: splittedInfo[0],
+          state: splittedInfo[3],
+          district: "",
+          city: splittedInfo[1],
+          zipcode: "",
+          lat: e.y,
+          lng: e.x,
+        }
+      } else if(splittedInfo.length == 9) {
+        this.address = {
+          street: splittedInfo[0],
+          district: splittedInfo[1],
+          city: splittedInfo[2],
+          state: splittedInfo[5],
+          zipcode: splittedInfo[7],
+          lat: e.y,
+          lng: e.x,
+        }
+      } else {
+        this.address['lat'] = e.y
+        this.address['lng'] = e.x
+      }
+    },
+    selectPosition(lat, lng){
+      if(!this.marker) {
+        this.marker = L.marker([lat, lng])
+        this.marker.addTo(this.map);
+      } else {
+        this.marker.setLatLng([lat, lng])
+      }
     }
   }
 }
