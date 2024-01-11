49f4fcdddf7901209129186bdf4cb861cd299621
jackson <jackson.primodev@gmail.com>
2024-01-11 01:38:41 -0300

    create loading component

---
+++ components/loading.vue
@@ -0,0 +1,46 @@
+<template>
+   <div class="loading-overlay">
+      <img src="../static/images/loading.gif" alt="Loading">
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
+.loading-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(255, 255, 255, 0.185);    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 9999;
+}
+
+.loading-overlay img {
+    width: 200px;
+    height: 200px;
+    animation: floatAnimation 1s infinite alternate;
+}
+
+@keyframes floatAnimation {
+  from {
+    transform: translateY(10px);
+  }
+  to {
+    transform: translateY(-10px);
+  }
+}
+
+</style>
